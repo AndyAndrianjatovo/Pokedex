6e3ae40558295349dcef5921d691e398ea3e8829
Andy Andrianjatovo <andy.andrianjatovoniaina@gmail.com>
2023-09-02 12:07:59 +0300

    init

---
+++ Dex/Models/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Dex.Models
+{
+    public class Pokemon
+    {
+        public int id { get; set; } = 0;
+        public string name { get; set; } = "";
+        public int weight { get; set; } = 0;
+        public int height { get; set; } = 0;
+        public Sprites sprites { get; set; } = new Sprites();
+        public List<Stat> stats { get; set; } = new List<Stat>();
+        public List<Type> types { get; set; } = new List<Type>();
+    }
+}
---
+++ Dex/Models/Type.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dex.Models
+{
+    public class Type
+    {
+        public int slot { get; set; }
+        public Type2 type { get; set; }
+    }
+}
