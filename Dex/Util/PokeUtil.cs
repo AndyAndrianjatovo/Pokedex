04bea940424bf5d6c13cb3d21db09bef5c3800bb
Andy Andrianjatovo <andy.andrianjatovoniaina@gmail.com>
2023-09-02 12:46:58 +0300

    fin dev v0 with data brute

---
+++ Dex/Util/PokeUtil.cs
@@ -0,0 +1,25 @@
+﻿namespace Dex.Util
+{
+    public static class PokeUtil
+    {
+        public const string Pokeball = "icons8-pokeball-96.png";
+        public const string GreatBall = "icons8-superball-96.png";
+        public const string UltraBall = "icons8-ultra-ball-96.png";
+        public const string MasterBall = "icons8-mega-ball-96.png";
+        public static string GetRandomBall()
+        {
+            var pokeballArray = new string[] { Pokeball, GreatBall, UltraBall, MasterBall };
+            var rand = new Random();
+            string randomBall = pokeballArray[rand.Next(pokeballArray.Length)];
+            return randomBall;
+        }
+        public static double GetPokemonHeightInMeters(int height)
+        {
+            return Convert.ToDouble(height) / 10;
+        }
+        public static double GetPokemonWeightInKilograms(int weight)
+        {
+            return Convert.ToDouble(weight) / 10;
+        }
+    }
+}
